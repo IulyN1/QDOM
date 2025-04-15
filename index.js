beaa271bb4b5824123aa9c2645cc463bdd2cd0ec
Iulian Popa <iulian.popa@garmin.com>
2025-04-15 11:34:44 +0300

    add select grammar

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("QDOM");
+import peggy from "peggy";
+import fs from "fs";
+
+const grammar = fs.readFileSync("./select.peggy", "utf8");
+const parser = peggy.generate(grammar);
+
+const input = "SELECT COUNT(*) FROM '#main';";
+const result = parser.parse(input);
+
+console.log(result);
